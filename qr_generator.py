f8ed8b699a0f0982307c77297bdd2bb5e0363b45
Raya Shahin <rayashahin08@gmail.com>
2025-12-11 21:35:42 +0200

    add data to the qr code

--- qr_generator.py
+++ qr_generator.py
@@ -1,7 +1,7 @@
 import qrcode
 from qrcode.constants import ERROR_CORRECT_H
 
-def generate_qr_code():
+def generate_qr_code(data, file_name):
     
     # add QR code instance
     qr = qrcode.QRCode(
@@ -10,3 +10,7 @@ def generate_qr_code():
         border = 4, 
         error_correction=ERROR_CORRECT_H # controls the error correction used in the QR code
     )
+
+    # add QR code data
+    qr.add_data(data)
+    qr.make(fit=True) # find the best size for the data
