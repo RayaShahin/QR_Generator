c3f8634ef04c4c7494f3a8e71447fe392f30e59d
Raya Shahin <rayashahin08@gmail.com>
2025-12-11 21:32:32 +0200

    add QR code instance

--- qr_generator.py
+++ qr_generator.py
@@ -2,4 +2,11 @@ import qrcode
 from qrcode.constants import ERROR_CORRECT_H
 
 def generate_qr_code():
-    pass
+    
+    # add QR code instance
+    qr = qrcode.QRCode(
+        version = 4, # size of the QR code
+        box_size = 12,
+        border = 4, 
+        error_correction=ERROR_CORRECT_H # controls the error correction used in the QR code
+    )
